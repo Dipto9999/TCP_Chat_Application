15ef77313248580015940f6cda48c1622c12636e
Muntakim Rahman <muntakim.rahman@gmail.com>
2024-11-20 22:24:31 -0800

    Init Part 2 Files

--- Part_2/main.py
+++ Part_2/main.py
@@ -3,14 +3,14 @@ from tkinter import *
 import multiprocessing
 import time
 
-import client
-import server
+import Part_2.client_part2 as client_part2
+import Part_2.server_part2 as server_part2
 
 if __name__ == "__main__":
-    server = multiprocessing.Process(target=server.main)
-    server.start()
+    server_part2 = multiprocessing.Process(target=server_part2.main)
+    server_part2.start()
     time.sleep(1)  #to ensure server is up and running; may be commented out or changed
 
     numberOfClients = 2  #Change this value for a different number of clients
     for count in range(1, numberOfClients+1):
-        multiprocessing.Process(target=client.main, name=f"Client{count}").start()
+        multiprocessing.Process(target=client_part2.main, name=f"Client{count}").start()
